31c921224d4b7dd2dc4fcebfe519541fdb27bd0f
Mohit Kumar Gond <mohitkg2601@gmail.com>
2023-06-14 13:55:00 +0530

    adding second one

---
+++ second/dom.js
@@ -0,0 +1,27 @@
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title="new";
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+
+
+// getelementby id
+var headerTitle=document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent="hello";
+// headerTitle.innerText="goodbye"; 
+//headerTitle.innerHTML='<h3>hello</h3>';
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px black';
+var title=document.getElementById('main');
+title.style.fontWeight='bold';
+title.style.color='green';
