234731a91828c5dda3ae1cf5737c01aef09a931c
Mohit Kumar Gond <mohitkg2601@gmail.com>
2023-06-14 16:17:59 +0530

    adding element by class name

--- second/dom.js
+++ second/dom.js
@@ -45,12 +45,19 @@
 
 
 //Get elements by tag name
-var li=document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent="hello2";
-// li[1].style.fontWeight='bold';
-// li[1].style.backgroundColor='yellow';
-for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor='yellow';
+// var li=document.getElementsByTagName('li');
+// // console.log(li);
+// // console.log(li[1]);
+// // li[1].textContent="hello2";
+// // li[1].style.fontWeight='bold';
+// // li[1].style.backgroundColor='yellow';
+// for(var i=0;i<li.length;i++){
+//     li[i].style.backgroundColor='yellow';
+// }
+
+
+//getelement by  class name
+var items=document.getElementsByClassName('list-group-item');
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='grey';
 }
