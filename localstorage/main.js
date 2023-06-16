5da9b7d10e62d3acde2552b5490c520ab6ccf6cf
Mohit Kumar Gond <mohitkg2601@gmail.com>
2023-06-16 18:11:19 +0530

    adding and delete local storage

--- localstorage/main.js
+++ localstorage/main.js
@@ -5,6 +5,9 @@ const msg=document.querySelector('.msg');
 const userList=document.querySelector('#users');
 
 myForm.addEventListener('submit', onSubmit);
+userList.addEventListener('click',removeIt);
+
+
 
 
 function onSubmit(e){
@@ -16,14 +19,37 @@ function onSubmit(e){
         li.appendChild(document.createTextNode(
             `${nameInput.value} : ${emailInput.value}`
         ));
+       
+        //Adding delete btn
+        var deleteBtn=document.createElement('button');
+        deleteBtn.className='btn btn-danger btn-sm float-right delete';
+
+        deleteBtn.appendChild(document.createTextNode('delete'));
+        li.appendChild(deleteBtn);
         userList.appendChild(li);
-        var name=JSON.stringify(nameInput.value);
+        //Adding local storage
+       
         
         localStorage.setItem(name,`name:${nameInput.value}  email:${emailInput.value}`);
-        
-        nameInput.vale='';
+        nameInput.value='';
         emailInput.value='';
     }
-    // var name=JSON.stringify(nameInput.value);
-    // console.log(name);
+   
+ 
+    
 }
+ var name=JSON.stringify(nameInput.value);
+
+//Remove items
+function removeIt(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li=e.target.parentElement;
+            userList.removeChild(li);
+            localStorage.removeItem(name);
+        }
+   }
+}
+
+  
+
